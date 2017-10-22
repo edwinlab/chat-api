902170cbd6f3f160920cb8fd987e34f9a4a6d4c9
Edwin Andrianto <edwinuad@gmail.com>
2017-10-22 22:26:15 +0700

    feat(socket.io): implement socket.io

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const logger = require('koa-logger');
 const mongoose = require('mongoose');
 const bodyParser = require('koa-bodyparser');
 const cors = require('kcors');
+const IO = require('socket.io');
 const routes = require('./routes');
 const config = require('./config');
 
@@ -20,5 +21,44 @@ const app = new Koa()
   .use(routes.allowedMethods());
 
 const server = app.listen(config.server.port);
+
+const io = IO(server);
+
+const onlineUsers = {};
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+
+  socket.on('userLogin', (username) => {
+    console.log('%s has joined the chat!', username);
+    onlineUsers[socket.id] = username;
+
+    io.emit('userJoin', {
+      socketId: socket.id,
+      username,
+      timestamp: new Date(),
+      type: 'alert',
+      message: `${username} has joined the chat!`,
+    });
+  });
+
+  socket.on('sendMessage', (message) => {
+    const senderUsername = onlineUsers[socket.id];
+
+    const messageObject = {
+      socketId: socket.id,
+      username: senderUsername,
+      timestamp: new Date(),
+      type: 'message',
+      message,
+    };
+
+    console.log('%s: %s', senderUsername, message);
+
+    io.emit('newMessage', messageObject);
+  });
+
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
 
 module.exports = server;
